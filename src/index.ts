20340671ca5143d4a344d35d58f3010111d30717
Christian Alexsander Santos <christianalexbh@hotmail.com>
2022-06-20 22:45:53 -0300

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export { default as withDecoratorInjection } from './withDecoratorInjection';
+export { default as withMergedDecorator } from './withMergedDecorator';
 export type {
   DecoratorInjectionProps,
   IApllyDecoratorInjection,
   IDecoratorInjectionFC,
+  IMergeDecorators,
 } from './withDecoratorInjectionTypes';
